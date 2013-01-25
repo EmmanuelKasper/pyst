a6d2472124333bb6155a05021cf8c78649056491
Emmanuel Kasper <emmanuel@libera.cc>
2013-01-25 19:04:43 +0100

    Refine the running loop

--- pyst.py
+++ pyst.py
@@ -6,13 +6,21 @@ from datetime import datetime
 # from the subprocess module, import the call function
 from subprocess import call
 # from the sys module, get command line arguments in the current name context
-from sys import argv
+from sys import argv,exit
 
 script, film = argv
 
-my_time = datetime.now()
-time_in_s = (my_time.hour * 3600) + (my_time.minute * 60) + my_time.second
-vlc_cli = "cvlc --no-osd --fullscreen --loop --start-time" 
-cmd_list = "%s %d %s" % (vlc_cli,time_in_s,film)
-call (cmd_list, shell=True)
+while 1:
+
+	now = datetime.now()
+	time_in_s = (now.hour * 3600) + (now.minute * 60) + now.second
+
+	vlc_cli = "cvlc --no-osd --fullscreen --play-and-exit --start-time" 
+	cmd_list = "%s %d %s" % (vlc_cli,time_in_s,film)
+
+	call (cmd_list, shell=True)
+
+sys.exit(0)
+
+
 
