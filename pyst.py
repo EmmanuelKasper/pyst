1e3c8ed2934f5f04212c47085e775b1572b725ee
Emmanuel Kasper <emmanuel@libera.cc>
2013-01-25 19:45:40 +0100

    Use VLC instead of cvlc

--- pyst.py
+++ pyst.py
@@ -15,7 +15,7 @@ while 1:
 	now = datetime.now()
 	time_in_s = (now.hour * 3600) + (now.minute * 60) + now.second
 
-	vlc_cli = "cvlc --no-osd --fullscreen --play-and-exit --start-time" 
+	vlc_cli = "vlc --no-osd --fullscreen --play-and-exit --start-time" 
 	cmd_list = "%s %d %s" % (vlc_cli,time_in_s,film)
 
 	call (cmd_list, shell=True)
